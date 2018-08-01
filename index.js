9d701b633433eed4e23b30cd1d06338f523a416e
Evan Solomon <evanlsolomon@gmail.com>
2018-08-01 20:28:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching (drivers,name){
   return drivers.filter(function driverName(driverName){
-    return driverName === name;
+    return driverName.toLowerCase() === name.toLowerCase();
   } );
 }
